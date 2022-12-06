5fbc6701de1669c0174ebdd5e1acfb506d0cf53e
user <user>
2022-12-06 11:28:24 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+
+ChristmasTree.Print();
+Console.ReadLine();
+class ChristmasTree
+{
+    internal static void Print()
+    {
+       
+        int size;
+        Write("Enter size:");
+        while (!int.TryParse(ReadLine(), out size) || size <= 0)
+            Write("Enter size, damn it: ");
+        for (int i = 1; i <= size; i++)
+            for (int j = 1; j <= i; j++)
+                WriteLine(new string(' ', size - j) + new string('6', j * 2 - 1));
+
+    }
+}
